45cc7bfa94f4d67ce820a70c2a50d7c16b36f51a
Robert <g00395612@atu.ie>
2023-10-08 11:07:52 +0100

    Initial Commit

---
+++ src/main/java/ie/atu/lab2_exercise3/Lab2Exercise3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.lab2_exercise3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2Exercise3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2Exercise3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab2_exercise3/Lab2Exercise3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab2_exercise3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2Exercise3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
